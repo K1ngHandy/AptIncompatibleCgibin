1860f42efd4d802141be8c986ff491c0a043c504
K1ngHandy <bolds-slicks0g@icloud.com>
2024-02-11 20:51:10 +0000

    frame link repair

--- index.js
+++ index.js
@@ -12,15 +12,19 @@ app.get("/", (req, res) => {
         <!-- Custom Meta Tags -->
         <meta name="fc:frame" content="vNext" />
         <meta name="fc:frame:image" content="https://fc-dev-call.replit.app/image">
+        
         <meta name="fc:frame:button:1" content="Spec" />
         <meta name="fc:frame:button:1:action" content="link" />
-        <meta name="fc:frame:button:1:target" content="https://docs.farcaster.xyz/reference/frames/spec#VPSidebarNav" />
+        <meta name="fc:frame:button:1:target" content="https://docs.farcaster.xyz/reference/frames/spec#constructing-a-frame" />
+        
         <meta name="fc:frame:button:2" content="Image" />
         <meta name="fc:frame:button:2:action" content="link" />
         <meta name="fc:frame:button:2:target" content="https://fc-dev-call.replit.app/image" />
+        
         <meta name="fc:frame:button:3" content="Frame Validator" />
         <meta name="fc:frame:button:3:action" content="link" />
         <meta name="fc:frame:button:3:target" content="https://warpcast.com/~/developers/frames" />
+        
         <meta name="fc:frame:button:4" content="Replit" />
         <meta name="fc:frame:button:4:action" content="link" />
         <meta name="fc:frame:button:4:target" content="https://replit.com/@kinghandy/AptIncompatibleCgibin#index.js" />
