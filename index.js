28bb7f13a24971871f294ad399813a7be3146968
K1ngHandy <bolds-slicks0g@icloud.com>
2024-02-11 20:28:25 +0000

    add tag urls

--- index.js
+++ index.js
@@ -8,28 +8,26 @@ app.get("/", (req, res) => {
     <html>
     <head>
         <title>My Node App</title>
-        <meta property="og:title" content="My Node App" />
-        <meta property="og:description" content="A simple Node.js app with OG tags" />
         <meta property="og:image" content="https://fc-dev-call.replit.app/image" />
         <meta property="og:url" content="https://example.com" />
         <!-- Custom Meta Tags -->
-        <meta name="fc:frame" content="exampleFrame">
+        <meta name="fc:frame" content="vNext">
         <meta name="fc:frame:image" content="https://fc-dev-call.replit.app/imagejpg">
-        <meta name="fc:frame:button:1" content="Button 1 Text">
-        <meta name="fc:frame:button:1:action" content="Action1">
-        <meta name="fc:frame:button:1:target" content="Target1">
-        <meta name="fc:frame:button:2" content="Button 2 Text">
-        <meta name="fc:frame:button:2:action" content="Action2">
-        <meta name="fc:frame:button:2:target" content="Target2">
-        <meta name="fc:frame:button:3" content="Button 3 Text">
-        <meta name="fc:frame:button:3:action" content="Action3">
-        <meta name="fc:frame:button:3:target" content="Target3">
-        <meta name="fc:frame:button:4" content="Button 4 Text">
-        <meta name="fc:frame:button:4:action" content="Action4">
-        <meta name="fc:frame:button:4:target" content="Target4">
+        <meta name="fc:frame:button:1" content="Spec">
+        <meta name="fc:frame:button:1:action" content="link">
+        <meta name="fc:frame:button:1:target" content="https://docs.farcaster.xyz/reference/frames/spec#VPSidebarNav">
+        <meta name="fc:frame:button:2" content="Image">
+        <meta name="fc:frame:button:2:action" content="link">
+        <meta name="fc:frame:button:2:target" content="https://fc-dev-call.replit.app/imagejpg">
+        <meta name="fc:frame:button:3" content="Frame Validator">
+        <meta name="fc:frame:button:3:action" content="link">
+        <meta name="fc:frame:button:3:target" content="https://warpcast.com/~/developers/frames">
+        <meta name="fc:frame:button:4" content="Replit">
+        <meta name="fc:frame:button:4:action" content="link">
+        <meta name="fc:frame:button:4:target" content="https://replit.com/@kinghandy/AptIncompatibleCgibin#index.js">
     </head>
     <body>
-        <h1>Welcome to My Node App</h1>
+        <h1>Frames Node App</h1>
     </body>
     </html>
   `;
